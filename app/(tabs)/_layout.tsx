db59aa4b71ebc2dc343339178eaf2d7640c7854a
Yogesh Palve <dnyyogesh@gmail.com>
2025-03-20 12:57:01 +0530

    minor fixes

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -75,10 +75,10 @@ export default function TabsLayout() {
       <Tabs.Screen
         name="save"
         options={{
-          title: "Save",
+          title: "Saved",
           headerShown: false,
           tabBarIcon: ({ focused }) => (
-            <TabIcon focused={focused} icon={icons.save} title="Save" />
+            <TabIcon focused={focused} icon={icons.save} title="Saved" />
           ),
         }}
       />
